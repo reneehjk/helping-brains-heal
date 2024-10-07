850acb88b3a71736b97fc3b099e12552941ffe01
Eve <126996217+EveYo@users.noreply.github.com>
2024-10-07 13:05:30 -0700

    Add fonts and update index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        color:{
+          1:"#121212", // font color black
+          2:"#125488", // primary dark blue 
+          3:"#5BA3ED", // secondary light blue
+          4:"F7F9FC", // background
+        }
+      },
+      borderWidth: {
+        DEFAULT: "8px",
+      },
+      fontFamily: {
+        satoshiBold: ['Satoshi-Bold', 'sans-serif'],
+        satoshiMedium: ['Satoshi-Medium', 'sans-serif'],
+        erodeRegular: ['Erode-Regular', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
