bd39caae4298049e6c7fb2e8ecc390dd229f01a0
kylezhao101 <kylezhao101@gmail.com>
2024-11-18 15:35:18 -0800

    Refractor communities page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,7 +4,7 @@ const NotFound = () => {
     return (
         <div className='h-screen text-center flex flex-col justify-center select-none'>
             <h1 className='font-satoshiBold'>404 - Page Not Found</h1>
-            <p className='font-erodeRegular mt-2'>Sorry, the page you’re looking for doesn’t exist.</p>
+            <p className='font-erodeRegular mt-2'>The page you’re looking for doesn’t exist.</p>
             <Link to="/" className='font-erodeRegular mt-5'>Go back to Home</Link>
         </div>
     );
