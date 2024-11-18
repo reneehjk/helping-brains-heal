69aa5bf1f5920f00fa6e06d14ffbf8cb9af60c62
Renee Kim <154100508+reneehjk@users.noreply.github.com>
2024-11-17 19:30:28 -0500

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div className='h-screen text-center flex flex-col justify-center'>
+        <div className='h-screen text-center flex flex-col justify-center select-none'>
             <h1 className='font-satoshiBold'>404 - Page Not Found</h1>
             <p className='font-erodeRegular mt-2'>Sorry, the page you’re looking for doesn’t exist.</p>
             <Link to="/" className='font-erodeRegular mt-5'>Go back to Home</Link>
