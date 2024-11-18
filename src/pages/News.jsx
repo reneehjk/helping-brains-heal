30a42f1ffb7674291eb8a41b24e2bdd05d198b5c
kylezhao101 <kylezhao101@gmail.com>
2024-11-18 15:10:09 -0800

    Refractor About news section to use-query

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -22,7 +22,7 @@ function News() {
     const { data: newsItems = [], isLoading } = useQuery({
         queryKey: ['newsItems'],
         queryFn: fetchNews,
-        staletime: 1000 * 60 * 5,
+        staleTime: 1000 * 60 * 5,
     })
 
     return (
