31d79a8adeabefb5efe4b2a68cc2748057cad5b7
Renee Kim <154100508+reneehjk@users.noreply.github.com>
2024-11-21 21:44:12 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -87,10 +87,10 @@ const Home = () => {
                 <div className="w-full mx-auto grid grid-cols-1 md:grid-cols-2 items-center">
                     {/* Hero Content */}
                     <div>
-                        <h1 className="text-4xl font-satoshiBold leading-tight pt-20 md:pt-0">
+                        <h1 className="text-5xl font-satoshiBold leading-tight pt-20 md:pt-0">
                             Helping Brains Heal
                         </h1>
-                        <p className="mt-4 text-lg text-gray-900 mr-8">
+                        <p className="mt-4 text-lg text-gray-900 mr-8 font-erodeRegular">
                             Bridging the gap between clinically accessible treatments and patients enduring symptoms of Brain Injury.
                         </p>
                         <a
