5d92135ed5be6b69545af2cc1ab71c149effb49b
Renee Kim <154100508+reneehjk@users.noreply.github.com>
2024-11-21 20:03:26 -0500

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -57,25 +57,25 @@ function About() {
             <div className="min-h-screen flex flex-col justify-start lg:space-y-16 space-y-6 mx-auto w-full max-w-7xl px-5 md:px-10 md:py-20">
                 <div className="flex items-end lg:space-x-12 space-x-3 lg:pb-24 md:mt-20 sm:mt-24 xs:mt-28">
                     <img src={logo} className="lg:w-20 lg:h-20 w-10 h-10 self-center" alt="Helping Brains Heal Logo" />
-                    <div className="font-satoshiBold xl:text-5xl lg:text-5xl md:text-3xl text-2xl">
+                    <div className="font-satoshiBold text-5xl">
                         About Helping Brains Heal
                     </div>
                 </div>
-                <div className="font-satoshiBold xl:text-4xl lg:text-4xl md:text-2xl text-xl">Overview</div>
+                <div className="font-satoshiBold xl:text-4xl lg:text-4xl text-3xl">Overview</div>
 
                 <div className="flex flex-col lg:mx-48">
-                    <div className="flex justify-self-start font-satoshiBold lg:text-xl md:text-lg text-md">Our Mission </div>
-                    <div className="flex self-center font-erodeRegular xl:text-lg lg:text-lg md:text-md text-sm text-center mt-3 lg:pb-24 sm:mx-5">
+                    <div className="flex justify-self-start font-satoshiBold xl:text-xl lg:text-xl text-lg">Our Mission </div>
+                    <div className="flex self-center font-erodeRegular xl:text-lg lg:text-lg text-base text-center mt-3 lg:pb-24 sm:mx-5">
                         Our mission is to enhance the accessibility of treatments for individuals enduring brain injuries. We strive to improve
                          access to brain injury rehabilitation resources by connecting patients with affordable and convenient treatment 
                          opportunities. We hope to make a difference during the earlier stages of recovery. 
                     </div>
                 </div>
 
-                <div className="font-satoshiBold lg:text-4xl md:text-2xl text-xl">Gallery</div>
+                <div className="font-satoshiBold xl:text-4xl lg:text-4xl text-3xl">Gallery</div>
                 <ReactImageGallery items={galleryImages} />
 
-                <div className="font-satoshiBold lg:text-4xl md:text-2xl text-xl">Latest News</div>
+                <div className="font-satoshiBold xl:text-4xl lg:text-4xl text-3xl">Latest News</div>
                 <div className="flex lg:flex-row lg:justify-between lg:space-x-10 flex-col justify-center space-y-4">
                     {isLoading ? (
                         <div className="w-full">
@@ -105,19 +105,19 @@ function About() {
                 </div>
                 <div className="flex justify-center">
                     <Link to="/news">
-                        <button className="bg-transparent border lg:text-base text-sm border-fontBlack px-4 lg:py-2 py-1 text-fontBlack rounded-full hover:bg-fontBlack hover:text-white transition duration-300">
+                        <button className="bg-transparent border xl:text-lg lg:text-lg text-base border-fontBlack px-4 lg:py-2 py-1 text-fontBlack rounded-full hover:bg-fontBlack hover:text-white transition duration-300">
                             View All
                         </button>
                     </Link>
                 </div>
                 <div className="flex justify-center lg:py-24 py-12">
                     <div className="flex flex-col items-center">
-                        <div className="font-satoshiBold lg:text-4xl md:text-2xl text-xl text-center">Support Recovery Efforts</div>
-                        <div className="font-erodeRegular xl:text-lg lg:text-lg md:text-md text-sm mt-6 lg:w-1/2 w-3/4 text-center">
+                        <div className="font-satoshiBold xl:text-4xl lg:text-4xl text-3xl text-center">Support Recovery Efforts</div>
+                        <div className="font-erodeRegular xl:text-lg lg:text-lg text-base mt-6 lg:w-1/2 w-3/4 text-center">
                             Your donation helps fund care packages and rehabilitation resources for individuals with acquired brain injuries.
                         </div>
                         <Link to="/donations">
-                            <button className="mt-6 bg-secondaryBlue lg:text-base text-sm font-satoshiMedium px-6 lg:py-2 py-1 text-background rounded-full hover:scale-110 transition duration-300">
+                            <button className="mt-6 bg-secondaryBlue xl:text-lg lg:text-lg text-base font-satoshiMedium px-6 lg:py-2 py-1 text-background rounded-full hover:scale-110 transition duration-300">
                                 Donate Now
                             </button>
                         </Link>
