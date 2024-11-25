743dd17569a84dcd940539d49f401b76fe4b18d2
kylezhao101 <kylezhao101@gmail.com>
2024-11-24 21:49:56 -0800

    Update subheader

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -1,17 +1,19 @@
 const TeamCard = ({ role, name, description, backgroundImage, link }) => {
     return (
-        <a href={link} target="_blank" rel="noopener noreferrer" className="block transition-all duration-75">
+        <a href={link} target="_blank" rel="noopener noreferrer" className="block transition-all hover:text-primaryBlue duration-100 ease-out hover:shadow-lg">
             <div className="rounded-lg overflow-hidden bg-white shadow-md">
                 {/* Image with rounded top corners */}
-                <div
-                    className="relative h-60 rounded-t-lg overflow-hidden"
-                    style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
-                >
-                    {/* Badge in the top-right corner */}
+                <div className="relative h-72 rounded-t-lg overflow-hidden">
+                    <img
+                        src={backgroundImage}
+                        alt={`${name}'s profile`}
+                        className="w-full h-full object-cover" // Adjust object-top to center on important parts
+                    />
                     <div className="absolute top-2 right-2 bg-black px-3 py-1 rounded-full text-xs font-semibold text-white font-satoshiBold z-10">
                         {role}
                     </div>
                 </div>
+
 
                 {/* Name and description below the image */}
                 <div className="p-4 text-left md:h-32">
